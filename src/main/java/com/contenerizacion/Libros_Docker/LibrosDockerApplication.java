ef59c1b9a5377badd0a9da1aa2dd0926e6c4227d
ggalvisgg <gabrielagalvisgg@gmail.com>
2025-04-07 15:36:57 -0500

    Agregar archivos iniciales del proyecto

---
+++ src/main/java/com/contenerizacion/Libros_Docker/LibrosDockerApplication.java
@@ -0,0 +1,13 @@
+package com.contenerizacion.Libros_Docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contenerizacion/Libros_Docker/LibrosDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contenerizacion.Libros_Docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
